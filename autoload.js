81c1b7ef715421918da47caff94ecfefe7552e3e
Evan-Leo <54826485+Evan-Leo@users.noreply.github.com>
2020-04-20 21:49:50 -0700

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -33,7 +33,7 @@ if (screen.width >= 768) {
 	]).then(() => {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
-			apiPath: "https://github.com/Evan-Leo/live2d_api/tree/c1/",
+			apiPath: "https://github.com/Evan-Leo/live2d_api/tree/customised/",
 			//cdnPath: "https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"
 		});
 	});
